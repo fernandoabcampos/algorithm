91deed13bb99ca7ce6c6e6effe86a041d2e3f1ac
Fernando Antonio Barbeiro Campos <fernandoabcampos@gmail.com>
2016-08-23 23:24:04 +0200

    Remove duplicated from unsorted linked list without using extra buffer (initial solution).

---
+++ src/cracking/code/interview/chapt02/I_RemoveDuplicatedFromUnsortedLinkedList.java
@@ -0,0 +1,108 @@
+package cracking.code.interview.chapt02;
+
+public class I_RemoveDuplicatedFromUnsortedLinkedList {
+	
+	static class Node {
+		Node next = null;
+		int data;
+		
+		public Node(int d){
+			data = d;
+		}
+
+		public void appendToTail(int d){
+			Node end = new Node(d);
+			Node n = this;
+			while(n.next!=null){
+				n = n.next;
+			}
+			
+			n.next = end;
+		}
+		
+		@Override
+		public String toString() {
+			return "Node [data=" + data + ", next="  +(next!=null?next.data : false) +"]";
+		}
+		
+	}
+	
+	/*
+	 * Removing duplicated nodes from Unsorted Linked List (consider to be a singly)
+	 * Follow up - use of temporary buffer is not allowed.
+	 */
+	
+	public static Node removeDuplicatedNodesFromLinkedList(Node head){
+		Node n = head;
+		while ( n.next != null ){
+			Node beingCompared = n;
+			Node previous = null;
+			while(beingCompared.next != null ){
+				Node navigator = beingCompared.next;
+				//System.out.println("Linked:  "+ n +", Linked 2: "+ navigator);
+				
+				if(n.data == navigator.data){
+					if(previous==null){
+						n.next = navigator.next;
+					}
+					else{
+						previous.next = navigator.next;
+					}
+					beingCompared = navigator;
+					continue;
+					
+				}
+				previous = navigator;
+				beingCompared = beingCompared.next;
+			}
+			
+			if(n.next!=null)
+			{
+				n = n.next;
+			}
+			else {
+				return n;
+			}
+		}
+		
+		return n;
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		Node head = new Node(1);
+		head.appendToTail(2);
+		head.appendToTail(3);
+		head.appendToTail(3);
+		head.appendToTail(4);
+		head.appendToTail(5);
+		head.appendToTail(6);
+		head.appendToTail(6);
+		head.appendToTail(7);
+		head.appendToTail(6);
+		head.appendToTail(8);
+		head.appendToTail(6);
+		head.appendToTail(9);
+		removeDuplicatedNodesFromLinkedList(head);
+		printResult(head);
+		
+		head = new Node(1);
+		head.appendToTail(1);
+		head.appendToTail(1);
+		head.appendToTail(1);
+		head.appendToTail(1);
+		removeDuplicatedNodesFromLinkedList(head);
+		printResult(head);
+	}
+	
+	static void printResult(Node head){
+		while(head.next!=null){
+			System.out.println(head.toString());
+			head = head.next;
+		}
+		System.out.println(head);
+	}
+
+}
